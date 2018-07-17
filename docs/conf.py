a97f219c5c8542cc3ff9c49e49490597ba8364ad
ojs1g14 <ojs1g14@soton.ac.uk>
2018-07-17 15:48:02 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,11 +16,11 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import unittest.mock
+import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = unittest.mock.Mock()
+    sys.modules[mod_name] = mock.Mock()
 
 
 # -- Project information -----------------------------------------------------
