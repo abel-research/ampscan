e5cfb2b4f3b4afe2d16899aa3721462f7a767fae
Joshua Steer <joshua.w.steer@gmail.com>
2018-07-18 20:51:53 +0100

    Moved the GUIs into a new folder

--- AmpScan/__init__.py
+++ AmpScan/__init__.py
@@ -5,7 +5,3 @@ Created on Thu Dec 15 13:50:41 2016
 @author: js22g12
 """
 
-from .core import AmpObject
-from .registration import registration
-from .AmpScanGUI import AmpScanGUI
-from .socketDesignGUI import socketDesignGUI
