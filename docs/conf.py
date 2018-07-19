3a089af3f5550d6424912ae25aa6cbfc632bbc4a
ojs1g14 <ojs1g14@soton.ac.uk>
2018-07-19 11:28:08 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,8 @@ sys.path.insert(0, os.path.abspath('.'))
 from unittest.mock import MagicMock
 
 MOCK_MODULE = ["sip", "PyQt5", "PyQt5.QtGui", "PyQt5.QtCore", "PyQt5.QtWidgets",
-                "PyQt5.QtWidgets.QWidget"]
+                "PyQt5.QtWidgets.QWidget", 
+                "matplotlib.backends.backend_qt5agg.FigureCanvasQTAgg"]
 for i in MOCK_MODULE: sys.modules[i] = MagicMock()
 
 #from mock import Mock as MagicMock
