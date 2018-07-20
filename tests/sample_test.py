54acc1dcff3c85fcfa529644a3820a5bd44252ac
Oliver Stocks <ojs1g14@soton.ac.uk>
2018-07-20 11:49:42 +0100

    update sample_test.py - add module to path

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -26,7 +26,7 @@ class TestBasicFunction(unittest.TestCase):
     
     def test_AmpScan_imports(self):
         modPath = os.path.abspath(os.pardir)+"\\AmpScan"
-        print(modPath)
+	sys.path.insert(0, modpath)
         self.assertFalse(False)
  
 if __name__ == '__main__':
