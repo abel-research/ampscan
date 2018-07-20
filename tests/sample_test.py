59d1e6d2e6e3bb7ad120bd722f48c706c3d395c1
ojs1g14 <ojs1g14@soton.ac.uk>
2018-07-20 09:04:25 +0100

    Update sample_test.py - add print statement to debug pipeline

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -3,6 +3,7 @@ import unittest
 class TestBasicFunction(unittest.TestCase):
     def test(self):
         self.assertTrue(True)
+        print("Running sample_test.py")
  
 if __name__ == '__main__':
     unittest.main()
