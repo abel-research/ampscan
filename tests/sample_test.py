b011c37b334acaa3de31bf4f7890ad3783931d79
ojs1g14 <ojs1g14@soton.ac.uk>
2018-07-20 09:29:23 +0100

    Update sample_test.py

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -1,11 +1,15 @@
 import unittest
  
 class TestBasicFunction(unittest.TestCase):
-    import numpy, scipy, matplotlib, vtk
     
     def test_running(self):
-        self.assertTrue(True)
         print("Running sample_test.py")
+        self.assertTrue(True)
+    
+    def test_imports(self):
+        import numpy, scipy, matplotlib, vtk
+        s = str(type(numpy))
+        self.assertEqual(s, "<class 'module'>")
  
 if __name__ == '__main__':
     unittest.main()
