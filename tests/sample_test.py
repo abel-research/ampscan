d6175e06f5bf305e502e8e6582b22fca9cab5459
ojs1g14 <ojs1g14@soton.ac.uk>
2018-07-20 15:29:14 +0100

    Update sample_test.py

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -9,7 +9,7 @@ class TestBasicFunction(unittest.TestCase):
         self.assertTrue(True)
     
     def test_python_imports(self):
-        import numpy, scipy, matplotlib, vtk
+        import numpy, scipy, matplotlib, vtk, AmpScan.core
         s = str(type(numpy))
         self.assertEqual(s, "<class 'module'>")
         s = str(type(scipy))
@@ -18,21 +18,12 @@ class TestBasicFunction(unittest.TestCase):
         self.assertEqual(s, "<class 'module'>")
         s = str(type(vtk))
         self.assertEqual(s, "<class 'module'>")
+        s = str(type(AmpScan.core))
+        self.assertEqual(s, "<class 'module'>")
 
     @unittest.expectedFailure
     def test_failure(self):
         s = str(type("string"))
-        self.assertEqual(s, "<class 'module'>")
-    
-    def test_AmpScan_imports(self):
-        #modPath = os.path.abspath(os.getcwd())
-        #sys.path.insert(0, modPath)
-        #print("Check add new path 0:")
-        #print(sys.path[0])
-        import AmpScan.core
-        s = str(type(AmpScan.core))
-        print(s)
-        s = str(type(AmpScan.core))
         self.assertEqual(s, "<class 'module'>")
 
  
