91a61b613c27d3eb67944996e2cff58f25cbd135
ojs1g14 <ojs1g14@soton.ac.uk>
2018-07-20 12:14:46 +0100

    Update sample_test.py - check path update

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -27,6 +27,8 @@ class TestBasicFunction(unittest.TestCase):
     def test_AmpScan_imports(self):
         modPath = os.path.abspath(os.pardir)+"\\AmpScan"
         sys.path.insert(0, modPath)
+        for i in sys.path:
+            print(i)
         self.assertFalse(False)
  
 if __name__ == '__main__':
