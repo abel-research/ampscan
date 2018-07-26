62f0e3e7577733c369ccb172df04a82571a01027
ojs1g14 <ojs1g14@soton.ac.uk>
2018-07-26 12:08:23 +0100

    Update sample_test.py

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -31,7 +31,7 @@ class TestBasicFunction(unittest.TestCase):
     @unittest.expectedFailure
     def test_import_stl(self):
         from AmpScan.core import AmpObject
-        from AmpScan.tests import sample_stl_sphere.stl as stlFile
+        from AmpScan.tests import sample_stl_sphere as stlFile
         Amp = AmpObject(stlFile)
         self.assertRaises(MemoryError)
 
