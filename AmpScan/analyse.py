3411b37a0096a365f9505640a1aba77c3eef88cd
Joshua Steer <joshua.w.steer@gmail.com>
2019-03-17 16:02:58 +0000

    Fixed edge order in create_slices

--- AmpScan/analyse.py
+++ AmpScan/analyse.py
@@ -48,7 +48,7 @@ class analyseMixin(object):
 
         """
         # Find the brim edges 
-        ind = np.where(self.faceEdges[:,1] == -99999)
+        ind = np.where(self.faceEdges[:,1] == -99999)[0]
         # Define max Z from lowest point on brim
         maxZ = self.vert[self.edges[ind, :], 2].min()
         fig = plt.figure()
@@ -121,43 +121,74 @@ class analyseMixin(object):
             # Select edges with one vertex above and one below the slice plane 
             validEdgeInd = np.where(np.logical_xor(ind[:,0], ind[:,1]))[0]
             validfE = self.faceEdges[validEdgeInd, :].astype(int)
-            g = defaultdict(set)
-            faceOrder = np.zeros(len(validEdgeInd), dtype=int)
-            # Run eularian path algorithm to order faces
-            for v, w in validfE:
-                g[v].add(w)
-                g[w].add(v)
-            v = validfE[0,0]
-            j=0
-            while True:
-                try:
-                    w = g[v].pop()
-                except KeyError:
-                    break
-                g[w].remove(v)
-                faceOrder[j] = v
-                j+=1
-                v = w
+            faceOrder = analyseMixin.logEuPath(validfE)
+#            g = defaultdict(set)
+#            faceOrder = np.zeros(len(validEdgeInd), dtype=int)
+#            # Run eularian path algorithm to order faces
+#            for v, w in validfE:
+#                g[v].add(w)
+#                g[w].add(v)
+#            v = validfE[0,0]
+#            j=0
+#            while True:
+#                try:
+#                    w = g[v].pop()
+#                except KeyError:
+#                    break
+#                g[w].remove(v)
+#                faceOrder[j] = v
+#                j+=1
+#                v = w
             # Get array of three edges attached to each face
             validEdges = self.edgesFace[faceOrder, :]
             # Remove the edge that is not intersected by the plane
             edges = validEdges[np.isin(validEdges, validEdgeInd)].reshape([-1,2])
             # Remove the duplicate edge from order 
             e = edges.flatten()
-            odx = np.argsort(e)
-            inds = np.arange(1, len(e), 2)
-            row = np.unravel_index(odx, e.shape)[0]
-            mask = np.ones(len(e), dtype=bool)
-            mask[row[inds]] = False
-            sortE = e[mask]
+#            odx = np.argsort(e)
+#            inds = np.arange(1, len(e), 2)
+#            row = np.unravel_index(odx, e.shape)[0]
+#            mask = np.ones(len(e), dtype=bool)
+#            mask[row[inds]] = False
+#            sortE = e[mask]
+            sortE = []
+            for ed in e:
+                if ed not in sortE:
+                    sortE.append(ed)
+            sortE.append(sortE[0])
             # Add first edge to end of array
-            sortE = np.append(sortE, sortE[0])
+#            sortE = np.append(sortE, sortE[0])
+            sortE = np.asarray(sortE)
             polyEdge = self.edges[sortE]
             EdgePoints = np.c_[self.vert[polyEdge[:,0], :], 
                                self.vert[polyEdge[:,1], :]]
             #Create poly from 
-            polys.append(analyseMixin.planeEdgeintersect(EdgePoints, plane, axis=axis))
+            polys.append(analyseMixin.planeEdgeIntersect_cy(EdgePoints, plane, axis))
         return polys
+    
+    @staticmethod
+    def logEuPath(arr):
+        vmax = arr.shape[0]
+        rows = list(range(vmax))
+        order = np.zeros([vmax], dtype=int)
+        i = 0
+        val = arr[i, 0]
+        nmax = vmax-1
+        for n in range(nmax):
+            del rows[i]
+            order[n] = val
+            i=0
+            xmax = vmax - n + 1
+            for x in rows: 
+                if arr[x, 0] == val:
+                    val = arr[x, 1]
+                    break
+                if arr[x, 1] == val:
+                    val = arr[x, 0]
+                    break
+                i+=1
+        order[n+1] = val
+        return order
     
 #    def create_slices_cy(self, slices, axis='Z'):
 #        """
@@ -202,7 +233,20 @@ class analyseMixin(object):
 ##            polys.append(analyseMixin.planeEdgeintersect(EdgePoints, plane, axis=axis))
 #            polys.append(planeEdgeIntersect_cy(EdgePoints, plane, 2))
 #        return polys
-
+    @staticmethod
+    def planeEdgeIntersect_cy(arr, plane, axisInd):
+        emax = arr.shape[0]
+        intersectPoints = np.zeros((emax, 3), dtype=np.float32)
+        intersectPoints[:, axisInd] = plane
+        for i in range(emax):
+            for j in range(2):
+                e1 = arr[i, j]
+                e2 = arr[i, axisInd]
+                e3 = arr[i, j+3]
+                e4 = arr[i, axisInd+3]
+                intersectPoints[i, j] = e1 + (plane - e2) * (e3 - e1) / (e4 - e2)
+        return intersectPoints
+    
     @staticmethod
     def planeEdgeintersect(edges, plane, axis=2):
         r"""
