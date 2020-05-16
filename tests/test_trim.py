c5d5d9be52c6ca457fc8ab9a720639fff9674055
JoshuaSteer <Joshua.Steer@soton.ac.uk>
2020-05-16 09:23:45 +0100

    Added test for three point trim method

--- tests/test_trim.py
+++ tests/test_trim.py
@@ -3,6 +3,7 @@ Testing suite for trim module
 """
 import unittest
 from util import get_path
+import numpy as np
 
 
 class TestTrim(unittest.TestCase):
@@ -40,3 +41,19 @@ class TestTrim(unittest.TestCase):
         h = 0
         self.amp.planarTrim(h, plane=2)
         self.assertLessEqual(self.amp.vert[:, 2].max(), h)
+
+    def test_trim_3(self):
+        """Tests the trim method of AmpObject by checking no vertices are above trim line"""
+        # Test no points are above 10
+        p0 = np.array([50, 50, 0])
+        p1 = np.array([50, -50, -40])
+        p2 = np.array([-50, 50, 10])
+        v0 = p1 - p0
+        v1 = p2 - p0
+        c = np.cross(v0, v1)
+        c = c/np.linalg.norm(c)
+        k = -np.multiply(c, p0).sum()
+        # planar values for each vert on face 
+        height = -(self.vert[:, 0]*c[0] + self.vert[:, 1]*c[0] + k)/c[2]
+        self.amp.threePointTrim(p0, p1, p2)
+        self.assertLessEqual(self.amp.vert[:, 2], height)
