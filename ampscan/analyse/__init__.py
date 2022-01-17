d26100bb2b61815f8fb371f024e3c16268607d22
Joshua Steer <Joshua.Steer@soton.ac.uk>
2022-01-17 08:58:11 +0000

    Update __init__.py

--- ampscan/analyse/__init__.py
+++ ampscan/analyse/__init__.py
@@ -1,5 +1,5 @@
 from .analyse import (calc_volume_closed, create_slices, calc_perimeter, calc_widths, calc_csa, est_volume, 
                       visualise_slices, plot_slices, MeasurementsOut, CMapOut)
-from .output import getPDF, generateRegBinsCsv, generateRegCsv
+from .output import getPDF, generateRegBinsCsv, generateRegCsv, generate_spec
 
 del analyse, output
